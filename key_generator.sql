6e5bbe5781ad8350a58cd973075988170b50f1f6
kellasaurusrex <44414988+kellasaurusrex@users.noreply.github.com>
2018-10-23 17:35:40 -0500

    Update key_generator.sql
    This auto-generates primary keys for the Training Request Form and the Confirmed Trainings.

---
+++ key_generator.sql
@@ -0,0 +1,15 @@
+create sequence gTRFNo 
+start with 15
+increment by 1;
+
+select gTRFNo.nextval from dual;
+
+create sequence gCTNo
+start with 16
+increment by 1;
+
+select gCTNo.nextval from dual;
+
+create sequence gIPTNo
+start with 16
+increment by 1;
