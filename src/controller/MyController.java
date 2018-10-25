a7669e87f868ee0bc95a439e7f653d45e4b9d6bf
Mark Gorewicz <mark_gorewicz@syntelinc.com>
2018-10-25 11:06:48 -0500

    update

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -14,9 +14,9 @@ public class MyController
 		return "AdminDashboard"; //returning view name
 	}
 	
-	@RequestMapping(value="/r")
+	/*@RequestMapping(value="/r")
 	public String showRequestorDashboard()
 	{
 		return "request"; //returning view name
-	}
+	}*/
 }
